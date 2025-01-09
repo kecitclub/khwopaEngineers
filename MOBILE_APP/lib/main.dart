5254a2b22ac6f13971b79bf42b95361b379baf33
31jay <jayadevtripathi@gmail.com>
2025-01-09 23:37:24 +0545

    api service

--- MOBILE_APP/lib/main.dart
+++ MOBILE_APP/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'tracking.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: const TrackingList(),
     );
   }
 }
